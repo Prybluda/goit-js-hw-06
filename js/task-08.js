afaec3b611e265db82a71606267a261b23335da7
Oleksandr Bry <134804799+Prybluda@users.noreply.github.com>
2023-09-25 16:22:47 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
+// console.log(form)
+form.addEventListener('submit', formUse);
 
+function formUse(event){
+    event.preventDefault();
+    // console.dir(event.currentTarget);
+    const {email, password} = event.currentTarget.elements;
+
+    if(email.value ==='' || password.value === ''){
+        alert ('Всі поля повинні бути заповнені!')
+    } else {
+
+    const data = {
+        email: email.value,
+        password: password.value
+       
+    };
+    console.log(data)
+    event.currentTarget.reset();
+    }
+}
