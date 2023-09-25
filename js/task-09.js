886c9b7f6c31cf28fc1631256fc215e5b689d8a1
Oleksandr Bry <134804799+Prybluda@users.noreply.github.com>
2023-09-25 17:09:21 +0200

    uyfuf

--- js/task-09.js
+++ js/task-09.js
@@ -2,4 +2,14 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+
+}
+const color = document.querySelector('.color');
+const body = document.querySelector('body');
+console.log(body)
+const changeColorBtn = document.querySelector('.change-color');
+changeColorBtn.addEventListener('click', clickBtn);
+function clickBtn(){
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
 }
